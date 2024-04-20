515858383f9364e8c31cbafdc3a523815ed2e295
jonambas <jon@jonambas.com>
2024-04-19 20:23:26 -0400

    chore: remove an unused fixture

--- apps/vite-react/panda.config.ts
+++ apps/vite-react/panda.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from '@pandacss/dev';
 import { pluginResponsiveVariants } from 'panda-plugin-crv';
-import { semanticTokens } from '../fixtures/tokens';
 
 export default defineConfig({
   preflight: true,
   include: ['./{src,pages,app}/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
-      semanticTokens,
       breakpoints: {
         verybig: '2000px',
       },
