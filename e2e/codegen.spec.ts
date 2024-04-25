f9ff683ebab1ed5d7aa31e9183307a3fbbb0b127
jonambas <jon@jonambas.com>
2024-04-25 13:34:58 -0400

    test: add e2e tests

--- e2e/codegen.spec.ts
+++ e2e/codegen.spec.ts
@@ -27,4 +27,16 @@ test.describe('codegen', () => {
       'text_black p_3 bg_red.200 opacity_1 md:opacity_0 lg:opacity_1',
     );
   });
+
+  test('compound variants', async ({ page }) => {
+    await expect(page.getByText('Compound variants').first()).toHaveClass(
+      'text_black p_3 bg_amber.400',
+    );
+  });
+
+  test('responsive compound variants', async ({ page }) => {
+    await expect(page.getByText('Responsive compound variants')).toHaveClass(
+      'text_black p_3 bg_indigo.500 md:bg_gray.200 lg:bg_green.200',
+    );
+  });
 });
